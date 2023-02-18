67a71a1c7ae76ecee8cdf90b0297fbb08cb2e9d6
dawei <811055695@qq.com>
2023-02-18 17:03:15 +0800

    添加功能

--- src/NumberFormat.php
+++ src/NumberFormat.php
@@ -1,8 +1,20 @@
 <?php
+
 namespace NumberFormat;
-class NumberFormat {
-    public function convert(int $num) : string {
-        return (string) $num;
-    }
+class NumberFormat
+{
+	public function convert(int $num): string
+	{
+		return (string) $num;
+	}
+
+	public static function strFirst(string $string, TypeEnum $type): string
+	{
+		return match ($type->value)
+		{
+			1 => ucfirst($string),
+			2 => lcfirst($string),
+		};
+	}
 }
 
---
+++ src/TypeEnum.php
@@ -0,0 +1,7 @@
+<?php
+namespace NumberFormat;
+enum TypeEnum : int
+{
+	case up = 1;
+	case lower = 2;
+}
